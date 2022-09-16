91d7bae33eee990fc86be59f243c7c255a870889
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2022-09-16 06:42:46 +0530

    part1

---
+++ src/rough.js
@@ -0,0 +1,116 @@
+// console.log("___________________________________________________________________________________");
+
+// const App = () => {
+//     return (
+//        <>
+//         <h1>Hello react js </h1>
+//        </>
+//     )
+//   }
+
+// console.log("___________________________________________________________________________________");
+
+// const App = () => {
+//     let name = "ram kumar "
+//     return (
+//        <>
+//         <h1>Hello {name} </h1>
+//        </>
+//     )
+//   }
+
+// console.log("___________________________________________________________________________________");
+
+// const App = () => {
+//     let name = <h1>This is heading one here </h1>
+//     return (
+//        <>
+//          {name}
+//        </>
+//     )
+//   }
+
+// // console.log("___________________________________________________________________________________");
+
+// const App = () => {
+//     function fullName(firstName,lastName){
+//       return firstName + lastName;
+//     }
+//     return (
+//        <>
+//           <h2>Full name is {fullName("ram", " Kumar ")} </h2>
+//        </>
+//     )
+//   }
+
+// // console.log("___________________________________________________________________________________");
+
+// const App = () => {
+//     return (
+//        <>
+//           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLg-in5KHEFuzi5A1DQdbRIHxf1EXOwF5q8Er4-VNBvIFTyFEummOqIQqTT2bTqtz7XV4&usqp=CAU" alt="" />
+//        </>
+//     )
+//   }
+
+// // console.log("___________________________________________________________________________________");
+
+// const App = () => {
+
+//     let url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLg-in5KHEFuzi5A1DQdbRIHxf1EXOwF5q8Er4-VNBvIFTyFEummOqIQqTT2bTqtz7XV4&usqp=CAU"
+//     return (
+//        <>
+//           <img src= {url} alt="" />
+//        </>
+//     )
+//   }
+
+// // console.log("___________________________________________________________________________________");
+
+// const App = () => {
+
+//     return (
+//        <>
+//           <ol>
+//             <li>item 1</li>
+//             <li>item 2</li>
+//             <li>item 3</li>
+//           </ol>
+//        </>
+//     )
+//   }
+
+// // console.log("___________________________________________________________________________________");
+
+// const App = () => {
+//     let items = ["item 1", "item 2", "item 3"];
+//     return (
+//        <>
+//           <ol>
+//             {items.map((item,i)=>{
+//              return <li key={i}> {item} </li>
+//             })}
+//           </ol>
+//        </>
+//     )
+//   }
+
+// // console.log("___________________________________________________________________________________");
+
+// const App = () => {
+//     let items = ["item 1", "item 2", "item 3"];
+//     let listItem = items.map((item,i)=>{
+//       return  <li  key={i}> {item} </li>
+//     })
+//     return (
+//        <>
+//           <ol>
+//             {listItem}
+//           </ol>
+//        </>
+//     )
+//   }
+
+// // console.log("___________________________________________________________________________________");
+
+// // console.log("___________________________________________________________________________________");
