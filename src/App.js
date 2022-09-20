95a43cdb3d4cf3104855f906d5750be167638c2a
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2022-09-20 14:04:17 +0530

    part8

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import React, { useEffect } from 'react';
-function Home1(){
-  useEffect(()=>{
-    console.log("useEffect is called in home1 components ");
-    return ()=> {console.log("HOme1 component is being unmounted ")}
-  },[])
-  return(
-    <>
-      <h1> Home 1 components </h1>
-    </>
-  )
-}
-
+import React from 'react'
+import axios from 'axios'
 const App = () => {
+ async function  getData(){
+  await  axios.get("https://jsonplaceholder.typicode.com/users")
+    .then((res)=>{
+      console.log(res.data);
+      console.log("code after the api call ");
+    })
+  }
   return (
-    <div>
-      <h1>App components</h1>
-      <Home1 />
-    </div>
+    <>
+      <h1>App component </h1>
+      <button onClick={getData}>get data </button>
+    </>
   )
 }
 
--- src/rough.js
+++ src/rough.js
@@ -1,284 +1,67 @@
-console.log("____________________________________________________________________");
+// console.log("______________________________________________________________________");
 
-import { BrowserRouter ,Routes, Route  } from "react-router-dom"
-import About from "./About"
-import Details from "./Details"
-import Home from "./Home"
-const App = () => {
-  return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element = {<Home />} />
-          <Route path="/about"  element = {<About />} />
-          <Route path="/detail" element = {<Details />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  )
-}
-
-console.log("____________________________________________________________________");
+// import React from 'react'
+// import axios from 'axios'
+// const App = () => {
+//   function  getData(){
+//     axios.get("https://jsonplaceholder.typicode.com/users")
+//     .then((res)=>{
+//       console.log(res.data);
+//     })
+//   }
+//   return (
+//     <>
+//       <h1>App component </h1>
+//       <button onClick={getData}>get data </button>
+//     </>
+//   )
+// }
 
 
-import { BrowserRouter ,Routes, Route, Link  } from "react-router-dom"
-import About from "./About"
-import Details from "./Details"
-import Home from "./Home"
-const App = () => {
-  return (
-    <div>
-      <BrowserRouter>
-        <Link to= "/">Home</Link>
-        <Link to= "/about">about</Link>
-        <Link to= "/detail">details</Link>
-        <Routes>
-          <Route path="/" element = {<Home />} />
-          <Route path="/about"  element = {<About />} />
-          <Route path="/detail" element = {<Details />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  )
-}
+// console.log("______________________________________________________________________");
 
-console.log("____________________________________________________________________");
+// import React from 'react'
+// import axios from 'axios'
+// const App = () => {
 
-
-import { BrowserRouter ,Routes, Route } from "react-router-dom"
-import About from "./About"
-import Details from "./Details"
-import Header from "./Header"
-import Home from "./Home"
-const App = () => {
-  return (
-    <div>
-      <BrowserRouter>
-      <Header />
-        <Routes>
-          <Route path="/" element = {<Home />} />
-          <Route path="/about"  element = {<About />} />
-          <Route path="/detail" element = {<Details />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  )
-}
+//   function  getData(){
+//     axios.get("https://jsonplaceholder.typicode.com/users")
+//     .then((res)=>{
+//       console.log(res.data);
+//       console.log("code after the api call ");
+//     })
+//   }
+//   return (
+//     <>
+//       <h1>App component </h1>
+//       <button onClick={getData}>get data </button>
+//     </>
+//   )
+// }
+// console.log("______________________________________________________________________");
 
 
-console.log("____________________________________________________________________");
-
-import React from 'react'
-import { Link } from 'react-router-dom';
-import './header.css'
-
-const Header = () => {
-  return (
-    <header className='header'>
-        <Link to= "/">Home</Link>
-        <Link to= "/about">about</Link>
-        <Link to= "/detail">details</Link>
-    </header>
-  )
-}
-
-console.log("____________________________________________________________________");
-
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-const About = () => {
-  return (
-    <div>
-      <h1>Home page </h1>
-      <Link to= "/"> <button>go to home page </button></Link>
-    </div>
-  )
-}
-
-console.log("____________________________________________________________________");
+// import React from 'react'
+// import axios from 'axios'
+// const App = () => {
+//  async function  getData(){
+//   await  axios.get("https://jsonplaceholder.typicode.com/users")
+//     .then((res)=>{
+//       console.log(res.data);
+//       console.log("code after the api call ");
+//     })
+//   }
+//   return (
+//     <>
+//       <h1>App component </h1>
+//       <button onClick={getData}>get data </button>
+//     </>
+//   )
+// }
 
 
-import React from 'react'
-import {  useNavigate } from 'react-router-dom'
-
-const About = () => {
-  const navigate = useNavigate()
-  function goToHomePage(){
-    navigate("/")
-  }
-  return (
-    <div>
-      <h1>Home page </h1>
-       <button onClick={goToHomePage}>go to home page </button>
-    </div>
-  )
-}
-
-console.log("____________________________________________________________________");
-
-import { useState } from "react"
-import { BrowserRouter ,Routes, Route } from "react-router-dom"
-import About from "./About"
-import Details from "./Details"
-import Header from "./Header"
-import Home from "./Home"
-const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-  function LoginStatusButton(){
-    if(isLoggedIn === true){
-      return <button onClick={()=>setIsLoggedIn(false)}> Logout </button>
-    }else{
-      return <button onClick={()=> setIsLoggedIn(true)}>Login</button>
-    }
-  }
-  return (
-    <div>
-      <BrowserRouter>
-      <Header />
-      <LoginStatusButton />
-        <Routes>
-          <Route path="/" element = {<Home />} />
-          <Route path="/about"  element = {<About />} />
-          <Route path="/detail" element = {<Details />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  )
-}
-
-console.log("____________________________________________________________________");
-
-import { useState } from "react"
-import { BrowserRouter ,Routes, Route, Navigate } from "react-router-dom"
-import About from "./About"
-import Details from "./Details"
-import Header from "./Header"
-import Home from "./Home"
-const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-  function LoginStatusButton(){
-    if(isLoggedIn === true){
-      return <button onClick={()=>setIsLoggedIn(false)}> Logout </button>
-    }else{
-      return <button onClick={()=> setIsLoggedIn(true)}>Login</button>
-    }
-  }
-  return (
-    <div>
-      <BrowserRouter>
-      <Header />
-      <LoginStatusButton />
-        <Routes>
-          <Route path="/" element = { <Home />} />
-          <Route path="/about"  element = {isLoggedIn === true ? <About /> : <Navigate to= "/" />} />
-          <Route path="/detail" element = { isLoggedIn === true ?  <Details /> : <Navigate to= "/" />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  )
-}
-
-console.log("____________________________________________________________________");
-
-import { useState } from "react"
-import { BrowserRouter ,Routes, Route, Navigate } from "react-router-dom"
-import About from "./About"
-import Details from "./Details"
-import Header from "./Header"
-import Home from "./Home"
-const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-  function LoginStatusButton(){
-    if(isLoggedIn === true){
-      return <button onClick={()=>setIsLoggedIn(false)}> Logout </button>
-    }else{
-      return <button onClick={()=> setIsLoggedIn(true)}>Login</button>
-    }
-  }
-  return (
-    <div>
-      <BrowserRouter>
-      <Header />
-      <LoginStatusButton />
-        <Routes>
-          <Route path="/" element = { <Home />} />
-          <Route path="/about"  element = {isLoggedIn === true ? <About /> : <Navigate to= "/" />} />
-          <Route path="/detail" element = { isLoggedIn === true ?  <Details /> : <Navigate to= "/" />} />
-          <Route path="*" element = {<h1> 404 page not found </h1>} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  )
-}
-
-console.log("____________________________________________________________________");
-
-import React, { useEffect, useState } from 'react';
-const App = () => {
-  const [state, setState] = useState(true);
-
-  useEffect(()=>{
-    console.log("useEffect is called ");
-  })
-
-  const changeState = () =>{
-    setState(!state)
-  }
-  return (
-    <div>
-      <h1>App components state = {state.toString()} </h1>
-      <button onClick={changeState}>change state </button>
-    </div>
-  )
-}
-
-console.log("____________________________________________________________________");
-
-import React, { useEffect, useState } from 'react';
-const App = () => {
-  const [state, setState] = useState(true);
-
-  useEffect(()=>{
-    console.log("useEffect is called ");
-  },[])
-
-  const changeState = () =>{
-    setState(!state)
-  }
-  return (
-    <div>
-      <h1>App components state = {state.toString()} </h1>
-      <button onClick={changeState}>change state </button>
-    </div>
-  )
-}
-console.log("____________________________________________________________________");
-
-import React, { useEffect } from 'react';
-function Home1(){
-  useEffect(()=>{
-    console.log("useEffect is called in home1 components ");
-    return ()=> {console.log("HOme1 component is being unmounted ")}
-  },[])
-  return(
-    <>
-      <h1> Home 1 components </h1>
-    </>
-  )
-}
-
-const App = () => {
-  return (
-    <div>
-      <h1>App components</h1>
-      <Home1 />
-    </div>
-  )
-}
-
-console.log("____________________________________________________________________");
+// console.log("______________________________________________________________________");
 
 
 
-console.log("____________________________________________________________________");
+// console.log("______________________________________________________________________");
